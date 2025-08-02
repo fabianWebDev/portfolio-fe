82827a083c29d3d865876893e8cebdc9438d66b7
fabianWebDev <fabiancava22@gmail.com>
2025-08-02 14:35:17 -0600

    Remove hola

--- src/hooks/useContact.ts
+++ src/hooks/useContact.ts
@@ -14,7 +14,6 @@ export const useContact = (): UseContactReturn => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
     const [success, setSuccess] = useState(false);
-    const hola = 'hola';
 
     const sendMessage = async (contactData: ContactMessage) => {
         try {
