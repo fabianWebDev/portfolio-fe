2864b85df6ebc44cbf2dde18f61bd23a2208bdf3
fabianWebDev <fabiancava22@gmail.com>
2025-08-07 22:46:15 -0600

    Styles update 03-03

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -10,9 +10,10 @@ const Description = () => {
                 I specialize in <span className={styles.highlight}>backend development with Python and Django</span>,
                 as well as <span className={styles.highlight}>frontend development with React</span>.
                 I enjoy turning complex ideas into clean, functional, and well-structured applications by combining
-                <span className={styles.highlight}>logic</span>, <span className={styles.highlight}>design</span>,
+                <span className={styles.highlight}> logic</span>, <span className={styles.highlight}>design</span>,
                 and <span className={styles.highlight}>best development practices</span>.
-                <br /><br />
+                <br />
+                <br />
                 <span className={styles.highlightBig}>Wizard of Code</span> isn’t just a catchy name —
                 it’s my work philosophy: <span className={styles.highlight}>solving problems efficiently, sustainably, and with purpose</span>.
                 I believe software is a tool to <span className={styles.highlight}>improve lives</span>,
