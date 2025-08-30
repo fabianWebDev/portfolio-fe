e86f6f790652d1f9d1a635246a411161aa86474c
fabianWebDev <fabiancava22@gmail.com>
2025-08-29 21:34:05 -0600

    Environmental changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174,
+    host: true,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:7175/api/',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
